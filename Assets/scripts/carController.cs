03bde7c5cb7a71418bda463cbb0877cfeb322941
pablo <pablo.saintamary@gmail.com>
2025-12-08 19:51:04 +0100

    Script w/ keyboard move del coche

---
+++ Assets/scripts/carController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+public class carController : MonoBehaviour
+{
+	public float carSpeed;
+	Vector3 position;
+	public float minPos;
+	public float maxPos;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	position.x += Input.GetAxis ("Horizontal") * carSpeed * Time.deltaTime;
+	if(position.x>maxPos){
+		position.x=maxPos;
+	}
+	
+	if(position.x<minPos){
+		position.x=minPos;
+
+}
+	
+	
+	transform.position = position;
+        
+    }
+}
