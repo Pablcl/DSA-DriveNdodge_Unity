aa2a8a7f396abd182823dd8dd987fb362bc4d9da
Pablo Casado <mondepablo456@gmail.com>
2025-12-09 11:30:15 +0100

    Acceleración de la carretera

--- Assets/scripts/trackMove.cs
+++ Assets/scripts/trackMove.cs
@@ -3,19 +3,19 @@ using System.Collections;
 
 public class trackMove : MonoBehaviour
 {
-    public float speed;
+    public float speed;      // Velocidad actual
+    public float acc;        // Aceleración por segundo
+
     Vector2 poscarretera;
-
-
-
-    void Start()
-    {
-        
-    }
 
     void Update()
     {
-	poscarretera = new Vector2(0, Time.time * speed);
+        // Aumenta la velocidad progresivamente
+        speed += acc * Time.deltaTime;
+
+        // Mueve la textura en el eje Y según la velocidad
+        poscarretera = new Vector2(0, Time.time * speed);
+
         GetComponent<Renderer>().material.mainTextureOffset = poscarretera;
     }
 }
