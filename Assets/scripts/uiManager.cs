8b56c3ca463457dcb876cc8414862bcfc6507fdd
pablo <pablo.saintamary@gmail.com>
2025-12-11 07:36:58 +0100

    pause game function

--- Assets/scripts/uiManager.cs
+++ Assets/scripts/uiManager.cs
@@ -16,6 +16,18 @@ public class uiManager : MonoBehaviour
     {
         
     }
+
+    public void Pause() {
+
+    if (Time.timeScale == 1) {
+        Time.timeScale = 0;
+    }
+
+    else if (Time.timeScale == 0) {
+        Time.timeScale = 1;
+    }
+
+
 }
 
 	
