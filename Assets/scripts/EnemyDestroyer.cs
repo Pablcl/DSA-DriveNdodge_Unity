6700d9afd3529dca8cb3408b3bf493fc6a2853ac
Pablo Casado <mondepablo456@gmail.com>
2025-12-11 07:24:28 +0100

    Eliminar enemy car después de que aparezca en pantalla juego

---
+++ Assets/scripts/EnemyDestroyer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class EnemyC : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D col){
+	if (col.gameObject.tag == "Enemy Car"){
+	  Destroy (col.gameObject);
+	}
+    }
+}
