a42fb2402347bf1660b9a3f5ec99c287e2ac5e36
pablo <pablo.saintamary@gmail.com>
2025-12-11 07:34:01 +0100

    splash screen + playscript + uimanager unity

---
+++ Assets/scripts/uiManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class uiManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    public void Play(){
+	Application.LoadLevel ("SampleScene");
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+	
+}
